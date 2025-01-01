5f083208e117f3d90a120b87999f4d624db24bb9
Josh <harrypotterhp333@gmail.com>
2025-01-01 16:28:27 -0700

    Initial commit, hero and about section changes

---
+++ components/About/index.tsx
@@ -0,0 +1,228 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <>
+      {/* <!-- ===== About Gifu Start ===== --> */}
+      <section className="overflow-hidden pb-20 lg:pb-25 xl:pb-30">
+        <div className="mx-auto max-w-c-1235 px-4 md:px-8 xl:px-0">
+          <div className="flex items-center gap-8 lg:gap-32.5">
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, x: -20 },
+                visible: { opacity: 1, x: 0 },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              transition={{ duration: 0.5, delay: 0.1 }}
+              viewport={{ once: true }}
+              className="animate_left relative mx-auto hidden aspect-square md:block md:w-1/2"
+            >
+              <Image
+                src="/images/about/aboutlight.gif"
+                alt="About Gifu Designs"
+                className="dark:hidden"
+                fill
+              />
+              <Image
+                src="/images/about/aboutdark.gif"
+                alt="About Gifu Designs"
+                className="hidden dark:block"
+                fill
+              />
+            </motion.div>
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, x: 20 },
+                visible: { opacity: 1, x: 0 },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              transition={{ duration: 0.5, delay: 0.1 }}
+              viewport={{ once: true }}
+              className="animate_right md:w-1/2"
+            >
+              
+              <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
+                Crafting Exceptional Websites for
+                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
+                  Modern Brands
+                </span>
+              </h2>
+              <p>
+                At Gifu Designs, we transform ideas into visually stunning and
+                highly functional websites. Based in Provo, Utah, our team
+                specializes in building custom designs that capture your brand’s
+                essence and elevate your online presence.
+              </p>
+
+              <div className="mt-7.5 flex items-center gap-5">
+                <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+                  <p className="text-metatitle2 font-semibold text-black dark:text-white">
+                    1
+                  </p>
+                </div>
+                <div className="w-3/4">
+                  <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
+                    Custom Designs
+                  </h3>
+                  <p>We tailor every design to meet your unique business needs.</p>
+                </div>
+              </div>
+              <div className="mt-7.5 flex items-center gap-5">
+                <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+                  <p className="text-metatitle2 font-semibold text-black dark:text-white">
+                    2
+                  </p>
+                </div>
+                <div className="w-3/4">
+                  <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
+                    User-Friendly Interfaces
+                  </h3>
+                  <p>Our designs focus on seamless user experiences.</p>
+                </div>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </section>
+      {/* <!-- ===== About Gifu End ===== --> */}
+
+      {/* <!-- ===== Our Approach Start ===== --> */}
+      <section>
+        <div className="mx-auto max-w-c-1235 overflow-hidden px-4 md:px-8 2xl:px-0">
+          <div className="flex items-center gap-8 lg:gap-32.5">
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, x: -20 },
+                visible: { opacity: 1, x: 0 },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              transition={{ duration: 1, delay: 0.1 }}
+              viewport={{ once: true }}
+              className="animate_left md:w-1/2"
+            >
+              <h2 className="relative mb-6 text-3xl font-bold text-black dark:text-white xl:text-hero">
+                Comprehensive Solutions for{" "}
+                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg2 dark:before:bg-titlebgdark">
+                  Your Online Presence
+                </span>
+              </h2>
+              <p>
+                At Gifu Designs, we handle everything you need to establish and
+                maintain a professional online presence. From seamless hosting and
+                domain management to setting up custom email servers, we’ve got you
+                covered.
+              </p>
+              <div className="mt-7.5">
+                <div className="flex items-center gap-5">
+                  <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+                    <p className="text-metatitle2 font-semibold text-black dark:text-white">
+                      1
+                    </p>
+                  </div>
+                  <div className="w-3/4">
+                    <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
+                      Reliable Hosting Services
+                    </h3>
+                    <p>Tailored solutions to ensure your website runs smoothly.</p>
+                  </div>
+                </div>
+                <div className="mt-7.5 flex items-center gap-5">
+                  <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+                    <p className="text-metatitle2 font-semibold text-black dark:text-white">
+                      2
+                    </p>
+                  </div>
+                  <div className="w-3/4">
+                    <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
+                      Domain Registration and Management
+                    </h3>
+                    <p>Effortless setup and management for your online identity.</p>
+                  </div>
+                </div>
+                <div className="mt-7.5 flex items-center gap-5">
+                  <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+                    <p className="text-metatitle2 font-semibold text-black dark:text-white">
+                      3
+                    </p>
+                  </div>
+                  <div className="w-3/4">
+                    <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
+                      Custom Email Servers
+                    </h3>
+                    <p>Professional communication with custom email solutions.</p>
+                  </div>
+                </div>
+                {/* <div className="mt-7.5 flex items-center gap-5">
+                  <div className="flex h-15 w-15 items-center justify-center rounded-[50%] border border-stroke dark:border-strokedark dark:bg-blacksection">
+                    <p className="text-metatitle2 font-semibold text-black dark:text-white">
+                      4
+                    </p>
+                  </div>
+                  <div className="w-3/4">
+                    <h3 className="mb-0.5 text-metatitle2 text-black dark:text-white">
+                      End-to-End Technical Support
+                    </h3>
+                    <p>Comprehensive support to handle all your technical needs.</p>
+                  </div>
+                </div> */}
+              </div>
+              <div>
+                <a
+                  href="#"
+                  className="group mt-7.5 inline-flex items-center gap-2.5 text-black hover:text-primary dark:text-white dark:hover:text-primary"
+                >
+                  <span className="duration-300 group-hover:pr-2">
+                    Learn More About Our Services
+                  </span>
+                  <svg
+                    width="14"
+                    height="14"
+                    viewBox="0 0 14 14"
+                    fill="currentColor"
+                  >
+                    <path d="M10.4767 6.16701L6.00668 1.69701L7.18501 0.518677L13.6667 7.00034L7.18501 13.482L6.00668 12.3037L10.4767 7.83368H0.333344V6.16701H10.4767Z" />
+                  </svg>
+                </a>
+              </div>
+            </motion.div>
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, x: 20 },
+                visible: { opacity: 1, x: 0 },
+              }}
+              initial="hidden"
+              whileInView="visible"
+              transition={{ duration: 1, delay: 0.1 }}
+              viewport={{ once: true }}
+              className="animate_right relative mx-auto hidden aspect-square md:block md:w-1/2"
+            >
+              <Image
+                src="/images/about/aboutlight2.gif"
+                alt="Hosting and Services"
+                className="dark:hidden"
+                fill
+              />
+              <Image
+                src="/images/about/aboutdark2.gif"
+                alt="Hosting and Services"
+                className="hidden dark:block"
+                fill
+              />
+            </motion.div>
+          </div>
+        </div>
+      </section>
+      {/* <!-- ===== Our Approach End ===== --> */}
+
+
+    </>
+  );
+};
+
+export default About;
---
+++ components/Hero/index.tsx
@@ -0,0 +1,74 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+const Hero = () => {
+  const [email, setEmail] = useState("");
+
+  // const handleSubmit = (e: React.FormEvent) => {
+  //   e.preventDefault();
+  //   alert(`Thank you for subscribing, ${email}!`);
+  // };
+
+  return (
+    <>
+      <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
+        <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
+          <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
+            <div className="md:w-1/2">
+              {/* <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
+                ✨ Elevate Your Web Presence with Gifu
+              </h4> */}
+              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero">
+              Designing Your Future <span> </span>
+                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark">
+                 Online Presence
+                </span>
+              </h1>
+              <p>
+                Gifu Design Co. specializes in innovative web solutions that bring your vision to life. Let’s build something extraordinary together!
+              </p>
+
+              <div className="mt-10">
+                  {/* <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    placeholder="Enter your email address"
+                    className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-none dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
+                  /> */}
+                <Link href="https://cal.com/gifu-dev/30min">
+                  <button
+                    aria-label="Schedule Meeting button"
+                    className="flex rounded-full bg-primary px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-primary-hover dark:bg-primary-dark dark:hover:bg-primary-hover"
+                  >
+                    Schedule Meeting
+                  </button>
+                </Link>
+
+                {/* <p className="mt-5 text-black dark:text-white">
+                  Stay updated with our latest design trends and tips.
+                </p> */}
+              </div>
+            </div>
+
+            <div className="animate_right hidden md:w-1/2 lg:block">
+              <div className="relative 2xl:-mr-7.5">
+                <Image
+                  src="/images/hero/MOCKUP 5 transparent.png"
+                  alt="Creative web design illustration"
+                  width={700}
+                  height={444}
+                  className="rounded-lg shadow-lg object-cover object-center h-96px"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
